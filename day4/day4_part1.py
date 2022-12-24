670c1bd11990e3427f41a00e4571a10bfa94b2fc
Aditya Kaushik <adityakaushik314@gmail.com>
2022-12-23 22:49:40 -0800

    day 4 of advent calendar

---
+++ day4/day4_part1.py
@@ -0,0 +1,9 @@
+f = open("day4_input.txt", 'r')
+count = 0
+for x in f.readlines():
+    int_pair = x.split(',')
+    start1, end1 = [int(a) for a in int_pair[0].split('-')]
+    start2, end2 = [int(a) for a in int_pair[1].split('-')]
+    if (start1 <= start2 and end1 >= end2) or (start2 <= start1 and end2 >= end1):
+        count += 1
+print(count)
